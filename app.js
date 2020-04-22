2f799545efacfd58fc89cd30af33a71f96ee44b3
Nicole Samanich <nicolesamanich@gmail.com>
2020-04-22 00:06:16 -0400

    fix for heroku push

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -8,7 +10,15 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const tiles = require("./routes/api/tiles");
 
+
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
