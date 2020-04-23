28ddb7f12a17c76321422a0659b4ea96728f7407
Nicole Samanich <nicolesamanich@gmail.com>
2020-04-23 12:41:20 -0400

    remove extra splash

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -7,11 +7,8 @@ import Timer from './timer/timer'
 import CreateRoomContainer from "./socket_test/create_room_container";
 import './css/index.css';
 
-
-
 const App = () => (
   <div className='container'>
-    <AuthRoute exact path="/" component={SplashContainer} />
     <Route exact path="/timer" component={Timer} />
     <AuthRoute exact path="/" component={SplashContainer} />
     <ProtectedRoute exact path="/create-room" component={CreateRoomContainer} />
