7b42b45a6973eb8ad446243579201ac7603527bf
Shrewbitz <swxg@umich.edu>
2020-05-13 18:14:24 -0400

    scoreboard sort

--- frontend/src/components/game_over/game_over.jsx
+++ frontend/src/components/game_over/game_over.jsx
@@ -9,25 +9,18 @@ class GameOver extends React.Component {
     }
 
     sortByPoints(player1, player2) {
-        // list.sort((a, b) => (a.color > b.color) ? 1 : (a.color === b.color) ? ((a.size > b.size) ? 1 : -1) : -1 )
         return Math.sign(player2.points - player1.points)
-        // list.sort((a, b) => (a.points > b.points) ? 1 : (a.color === b.color) ? ((a.size > b.size) ? 1 : -1) : -1 )
     }
 
 
 
     render() {
-        // this.props.gameState.players
-        // .points
-        // .handle
         const playersArray = Object.values(this.props.gameState.players).sort(this.sortByPoints);
-        //sortByPoints(playersArray)
 
         const scores = playersArray.map((player, idx) => {
             return (<li key={idx}> {player.handle} : {player.points} </li>)
         })
 
-        // debugger
         return (
             <div>
                 <h1>GAME OVER</h1>
