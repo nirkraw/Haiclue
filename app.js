5f20c76fac79761adca0398aae53cf98eec25cf9
nirkraw <nirkaw@gmail.com>
2020-05-18 12:35:33 -0400

    Co-authored-by: Edward Trinidad <edward.trinidad132@gmail.com>

--- app.js
+++ app.js
@@ -112,10 +112,10 @@ io.on("connect", (socket) => {
  ///////////////////////////////
   socket.on("demo", (handle, roomName) => { // roomName will be demo -will this be an issue with two instances of demos goign on concurenetly
     const demoRoom = new DemoRoom(roomName); // it can be demo because no one can create a lowercase room name
+    rooms[roomName] = demoRoom;
     demoRoom.addPlayer(handle);
     demoRoom.addPlayer("Khaleel");
     demoRoom.addPlayer("Will");
-    rooms[roomName] = demoRoom;
     rooms[roomName].storeTiles(tiles);
     rooms[roomName].startGame();
   });
