31d4321fa202c4b03c0259e2d661d60552027015
etrinidad <edward.trinidad132@gmail.com>
2020-05-21 18:45:40 -0400

    cleanup continued

--- frontend/src/components/timer/timer.jsx
+++ frontend/src/components/timer/timer.jsx
@@ -1,67 +1,75 @@
-import React from 'react';
-import '../css/timer.css';
+import React from "react";
+import "../css/timer.css";
 
 class Timer extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            secs: this.props.secs, 
-            timeClass: ""
+  constructor(props) {
+    super(props);
+    this.state = {
+      secs: this.props.secs,
+      timeClass: "",
+    };
+  }
 
-        };
-    }
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      if (this.state.secs === 0) {
+        clearInterval(this.interval);
+        //your pretty socket code here!
+        if (this.props.phase === "submit clue") {
+          this.props.socket.emit(
+            this.props.phase,
+            this.props.roomName
+          );
+        }
 
-    componentDidMount() {
-        this.interval = setInterval(() => {
-            if (this.state.secs === 0) {
-                clearInterval(this.interval)
-                //your pretty socket code here!
-                // debugger
-                if (this.props.phase === 'submit clue') {
-                 this.props.socket.emit(this.props.phase, this.props.roomName, this.props.handle, ); 
-                }
+        if (this.props.phase === "submit guess") {
+          const {
+            socket,
+            phase,
+            roomName,
+            localPlayerSocketId,
+            currentPlayerSocketId,
+          } = this.props;
 
-                if (this.props.phase === 'submit guess') {
-                    const { socket, phase, roomName, localPlayerHandle, currentPlayerHandle } = this.props
+          socket.emit(
+            phase,
+            roomName,
+            localPlayerSocketId,
+            false,
+            currentPlayerSocketId,
+            "too late..."
+          );
+        }
 
-                //  this.props.socket.emit(this.props.phase, this.props.roomName, this.props.handle, ); 
-                 socket.emit(phase, roomName, localPlayerHandle, false, currentPlayerHandle)
-                }
+        const timesUpSound = document.getElementById("time-up-sound");
+        timesUpSound.volume = 0.2;
+        timesUpSound.play();
+      } else if (this.state.secs < 7 && this.state.secs % 2 === 0) {
+        const countDown = document.getElementById("countdown");
+        countDown.volume = 0.4;
+        countDown.currentTime = 0;
+        countDown.play();
+        this.setState({
+          secs: this.state.secs - 1,
+          timeClass: "timer-flash",
+        });
+      } else {
+        this.setState({
+          secs: this.state.secs - 1,
+          timeClass: "",
+        });
+      }
+    }, 1000);
+  }
 
-                const timesUpSound = document.getElementById("time-up-sound");
-                timesUpSound.volume = .4;
-                timesUpSound.play();
+  componentWillUnmount() {
+    clearInterval(this.interval); //safety precaution
+  }
 
-
-            } else if ((this.state.secs < 7) && (this.state.secs % 2 === 0)) {
-                const countDown = document.getElementById("countdown");
-                countDown.volume = .4;
-                countDown.currentTime = 0;
-                countDown.play()
-                this.setState({
-                    secs: (this.state.secs - 1),
-                    timeClass: "timer-flash",
-                })
-            } else {
-                this.setState({
-                    secs: (this.state.secs - 1),
-                    timeClass: "",
-                })
-            }
-        }, 1000)
-    }
-
-
-    componentWillUnmount() {
-        clearInterval(this.interval) //safety precaution
-    }
-
-    render() {
-        let secs = this.state.secs
-        let timeClass = this.state.timeClass 
-        return (
-            <div className={`timer  ${timeClass}`} >time left {secs} </div>
-        )
-    }
+  render() {
+    let secs = this.state.secs;
+    let timeClass = this.state.timeClass;
+    return <div className={`timer  ${timeClass}`}>time left {secs} </div>;
+  }
 }
 export default Timer;
