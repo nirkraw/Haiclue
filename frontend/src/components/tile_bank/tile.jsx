cff9b6b6c020fde1abb71a6eb21431747cc16ab4
etrinidad <edward.trinidad132@gmail.com>
2020-05-21 18:26:11 -0400

    cleanup continued

--- frontend/src/components/tile_bank/tile.jsx
+++ frontend/src/components/tile_bank/tile.jsx
@@ -5,52 +5,37 @@ class Tile extends React.Component {
     super(props);
 
     this.clueSubmit = this.clueSubmit.bind(this);
-    this.playSound = this.playSound.bind(this);
   }
 
   clueSubmit(event) {
     const { roomName, player, tile } = this.props;
     event.preventDefault();
     if (player.clueTiles.includes(tile)) {
-      this.props.socket.emit("select clue tile",roomName, tile);
+      this.props.socket.emit("select clue tile", roomName, tile);
     } else {
       this.props.socket.emit("remove clue tile", roomName, tile);
     }
 
     const sound = document.getElementById("tile-sound");
     sound.currentTime = 0;
-    sound.volume = .35;
+    sound.volume = 0.35;
     sound.play();
   }
-
-  playSound() {
-    const sound = document.getElementById("tile-sound");
-    sound.currentTime = 0;
-    sound.volume = .35;
-    sound.play();
-  }
-
-
 
   render() {
     let currentColor = this.props.currentColor;
     let tileWord = this.props.tile[currentColor];
 
-    if(this.props.phase === "clue guessing") {
+    if (this.props.phase === "clue guessing") {
       return (
         <>
-        {this.props.display ? (
-          <div
-           className={`color-${currentColor} tile`}
-          //  onClick={this.playSound}
-          >
-            {tileWord}
-          </div>
-        ) : (
-          <div className="emptyTileSpace">{null}</div>
-        )}
-      </>
-      )
+          {this.props.display ? (
+            <div className={`color-${currentColor} tile`}>{tileWord}</div>
+          ) : (
+            <div className="emptyTileSpace">{null}</div>
+          )}
+        </>
+      );
     } else {
       return (
         <>
