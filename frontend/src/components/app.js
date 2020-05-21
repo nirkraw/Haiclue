2afec9e5992814a6e9e5b91782ee49fabb7e4ec1
etrinidad <edward.trinidad132@gmail.com>
2020-05-21 18:47:19 -0400

    cleanup continued

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import { Switch, Route } from 'react-router-dom';
-import SplashContainer from './splash/splash_container';
-import Timer from './timer/timer'
+import React from "react";
+import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import { Switch, Route } from "react-router-dom";
+import SplashContainer from "./splash/splash_container";
+import Timer from "./timer/timer";
 import CreateRoomContainer from "./create_room/create_room_container";
-import ExtInstructions from './splash/external-instructions';
-import './css/index.css';
+import ExtInstructions from "./splash/external-instructions";
+import "./css/index.css";
 
 const App = () => (
-  <div className='container'>
-    <Route exact path="/timer" component={Timer} />     
+  <div className="container">
+    <Route exact path="/timer" component={Timer} />
     <Switch>
       <Route exact path="/instructions" component={ExtInstructions} />
       <AuthRoute path="/" component={SplashContainer} />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,55 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './components/root';
-import configureStore from './store/store';
-import jwt_decode from 'jwt-decode';
-import { setAuthToken } from './util/session_api_util';
-import { logout } from './actions/session_actions';
-import { fetchTiles} from './actions/tile_actions'
+import React from "react";
+import ReactDOM from "react-dom";
+import Root from "./components/root";
+import configureStore from "./store/store";
+import jwt_decode from "jwt-decode";
+import { setAuthToken } from "./util/session_api_util";
+import { logout } from "./actions/session_actions";
+import { fetchTiles } from "./actions/tile_actions";
 
+document.addEventListener("DOMContentLoaded", () => {
+  let store;
 
-document.addEventListener('DOMContentLoaded', () => {
-      let store;
+  if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+    const decodedUser = jwt_decode(localStorage.jwtToken);
+    const preloadedState = {
+      session: {
+        isAuthenticated: true,
+        user: decodedUser, // id
+      },
+    };
 
+    store = configureStore(preloadedState);
 
-      if (localStorage.jwtToken) {
-        setAuthToken(localStorage.jwtToken);
-        const decodedUser = jwt_decode(localStorage.jwtToken);
-        const preloadedState = {
-          session: {
-            isAuthenticated: true,
-            user: decodedUser// id
-          }
-        };
+    const currentTime = Date.now() / 1000;
 
-        store = configureStore(preloadedState); 
-        
-        const currentTime = Date.now() / 1000;
-        
-        // If the user's token has expired
-        if (decodedUser.exp < currentTime) {
-          // Logout the user and redirect to the login page
-          store.dispatch(logout());
-          window.location.href = '/login';
-        }
-      } else {
-        // If this is a first time user, start with an empty store
-        store = configureStore({});
-      }
-      
-      window.fetchTiles = fetchTiles;
-      window.store = store;
-      window.getState = store.getState;
-      window.dispatch = store.dispatch;
+    // If the user's token has expired
+    if (decodedUser.exp < currentTime) {
+      // Logout the user and redirect to the login page
+      store.dispatch(logout());
+      window.location.href = "/login";
+    }
+  } else {
+    // If this is a first time user, start with an empty store
+    store = configureStore({});
+  }
 
-        const root = document.getElementById('root');
-        ReactDOM.render( <Root store = { store }/>, root);
-      }
-);
-      
-      
-      
-      
-  
-      
-      
+  window.fetchTiles = fetchTiles;
+  window.store = store;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
+  const root = document.getElementById("root");
+  ReactDOM.render(<Root store={store} />, root);
+});
