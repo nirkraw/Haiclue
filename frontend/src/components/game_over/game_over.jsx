467253090d70f7791c4e6f1e59b01698cbf0d80f
etrinidad <edward.trinidad132@gmail.com>
2020-05-21 17:25:43 -0400

    cleanup continued

--- frontend/src/components/game_over/game_over.jsx
+++ frontend/src/components/game_over/game_over.jsx
@@ -1,68 +1,77 @@
-import React from 'react';
-import '../css/game_over.css';
-import {NavLink} from 'react-router-dom';
-
+import React from "react";
+import "../css/game_over.css";
 
 class GameOver extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-        };
-        this.sortByPoints = this.sortByPoints.bind(this)
-        this.playAgain = this.playAgain.bind(this);
-        this.newRoom = this.newRoom.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.sortByPoints = this.sortByPoints.bind(this);
+    this.playAgain = this.playAgain.bind(this);
+    this.newRoom = this.newRoom.bind(this);
+  }
 
-    componentDidMount() {
-        const sound = document.getElementById("game-over-sound");
-        sound.volume = .4;
-        sound.play();
-    }
+  componentDidMount() {
+    const sound = document.getElementById("game-over-sound");
+    sound.volume = 0.4;
+    sound.play();
+  }
 
-    sortByPoints(player1, player2) {
-        return Math.sign(player2.points - player1.points)
-    }
+  sortByPoints(player1, player2) {
+    return Math.sign(player2.points - player1.points);
+  }
 
-    playAgain() {
-        this.props.socket.emit("play again", this.props.gameState.roomName);
-    }
+  playAgain() {
+    this.props.socket.emit("play again", this.props.gameState.roomName);
+  }
 
-    newRoom() {
-        window.location.reload();
-    }
+  newRoom() {
+    window.location.reload();
+  }
 
-  
-    render() {
-        const playersArray = Object.values(this.props.gameState.players).sort(this.sortByPoints);
-        
-        const scores = playersArray.map((player, idx) => {
-            return (<li key={idx}> {player.handle} <span className='points color-blue'> {player.points}</span> </li>)
-        })
-        let tie = [playersArray[0].handle]
-        for (let i = 1; i < playersArray.length; i++) {
-             if (playersArray[i].points === playersArray[0].points) {
-                 tie.push(playersArray[i].handle)
-             }
-        }
-        const winner = (
-            (tie.length > 1) 
-            ? <h1>{tie.slice(0, tie.length - 1).join(", ")} and {tie[tie.length - 1]} tie!</h1>
-            : <h1>{playersArray[0].handle} wins!</h1> 
-        )
-       
-        /// needs more logic to handle ties
-        return (
-            <div className='game-over-container'>
-                <h1 className='color-yellow game-over'>GAME OVER</h1>
-                {winner}
-                {/* <h1>{playersArray[0].handle} wins!</h1> */}
-                <ul>{scores}</ul>
-                <div className="game-over-buttons">
-                    <button onClick={this.playAgain} className='game-over-button'>Play Again!</button>
-                    <button onClick={this.newRoom} className='game-over-button'>New Room</button>
-                </div>
-            </div>
-        )
+  render() {
+    const playersArray = Object.values(this.props.gameState.players).sort(
+      this.sortByPoints
+    );
+
+    const scores = playersArray.map((player, idx) => {
+      return (
+        <li key={idx}>
+          {player.handle}
+          <span className="points color-blue"> {player.points}</span>{" "}
+        </li>
+      );
+    });
+    let tie = [playersArray[0].handle];
+    for (let i = 1; i < playersArray.length; i++) {
+      if (playersArray[i].points === playersArray[0].points) {
+        tie.push(playersArray[i].handle);
+      }
     }
+    const winner =
+      tie.length > 1 ? (
+        <h1>
+          {tie.slice(0, tie.length - 1).join(", ")} and {tie[tie.length - 1]}{" "}
+          tie!
+        </h1>
+      ) : (
+        <h1>{playersArray[0].handle} wins!</h1>
+      );
+
+    return (
+      <div className="game-over-container">
+        <h1 className="color-yellow game-over">GAME OVER</h1>
+        {winner}
+        <ul>{scores}</ul>
+        <div className="game-over-buttons">
+          <button onClick={this.playAgain} className="game-over-button">
+            Play Again!
+          </button>
+          <button onClick={this.newRoom} className="game-over-button">
+            New Room
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 export default GameOver;
--- frontend/src/components/my_target_word/my_target_word.jsx
+++ frontend/src/components/my_target_word/my_target_word.jsx
@@ -9,57 +9,53 @@ import "../css/my_target_word.css";
 class MyTargetWord extends React.Component {
   constructor(props) {
     super(props);
-  
     this.getWord = this.getWord.bind(this);
   }
 
   getWord() {
-    const {gameState, user} = this.props 
-    if(!gameState) return null;
-    
-    const player = Object.values(gameState.players).filter(
-      (player) => {
-        return player.handle === user.handle;
-      }
-    )[0];
-
-       const currentColor = gameState.currentColor;
-       if(player.targetWord) {
-         const targetWord = player.targetWord;
-         return targetWord[currentColor];
-       } else {
-         return " ";
-       }
-  }
-
-  
-  render() {
-    const cards = [
-      blue,
-      red,
-      green,
-      yellow,
-    ];
-
-    const {gameState} = this.props 
+    const { gameState, user } = this.props;
     if (!gameState) return null;
 
-    if(gameState.over) return (<></>)
+    const player = Object.values(gameState.players).filter((player) => {
+      return player.handle === user.handle;
+    })[0];
+
+    const currentColor = gameState.currentColor;
+    if (player.targetWord) {
+      const targetWord = player.targetWord;
+      return targetWord[currentColor];
+    } else {
+      return " ";
+    }
+  }
+
+  render() {
+    const cards = [blue, red, green, yellow];
+
+    const { gameState } = this.props;
+    if (!gameState) return null;
+
+    if (gameState.over) return <></>;
 
     let myWord = this.getWord();
     let picNumber;
     for (let i = 0; i < 4; i++) {
-      if (gameState.targetWords[i] === undefined){
-        this.props.logout();//send me to splash
+      if (gameState.targetWords[i] === undefined) {
+        this.props.logout(); //send me to splash
         window.location.reload();
-      } 
+      }
 
-      if (gameState.targetWords[i][gameState.currentColor] === myWord) 
-      { picNumber = i}
+      if (gameState.targetWords[i][gameState.currentColor] === myWord) {
+        picNumber = i;
+      }
     }
     return (
       <div className="my-target-word-container">
-        <img src={cards[picNumber]} className="my-target-word-img" alt="green" /> 
+        <img
+          src={cards[picNumber]}
+          className="my-target-word-img"
+          alt="green"
+        />
         <div className={`color-${gameState.currentColor} tile`}>{myWord}</div>
       </div>
     );
